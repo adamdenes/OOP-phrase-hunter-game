f71ce09829c99e68d6a1a1bab9d2c42576bbc740
Adam Denes <adamdenes@icloud.com>
2020-10-21 16:29:54 +0200

    rolling

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,54 @@
 # Create your Game class logic in here.
+import random
+from phrasehunter.phrase import Phrase
+
+
+class Game():
+
+    def __init__(self):
+        self.missed = 0
+        self.phrases = [
+            Phrase('What Goes Up Must Come Down'),
+            Phrase('Hard Pill to Swallow'),
+            Phrase('Right Off the Bat'),
+            Phrase('Down And Out'),
+            Phrase('Man of Few Words')
+        ]
+        self.active_phrase = self.get_random_phrase()
+        self.guesses = [' ']
+
+    def start(self):
+        self.welcome()
+
+        while self.missed <= 5:
+            print('Number missed: {}'.format(self.missed))
+            print()
+            self.active_phrase.display(self.guesses)
+            print()
+
+            user_guess = self.get_guess()
+            self.guesses.append(user_guess)
+
+            if not self.active_phrase.check_letter(user_guess):
+                self.missed += 1
+
+    def get_random_phrase(self):
+        rand_numb = random.randint(0, len(self.phrases)-1)
+        return self.phrases[rand_numb]
+
+    def welcome(self):
+        welcome = 'Welcome to Phrase Hunter!'
+        empty = ' ' * len(welcome)
+        print('#############################')
+        print('# {} #'.format(empty))
+        print('# {} #'.format(welcome))
+        print('# {} #'.format(empty))
+        print('#############################')
+        print()
+
+    def get_guess(self):
+        guess = input('Guess a letter: ')
+        return guess
+
+    def game_over(self):
+        pass
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,22 @@
 # Create your Phrase class logic here.
+class Phrase():
+
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+    
+    def display(self, guesses):
+        for i in range(len(guesses)):
+            for j in range(len(self.phrase)):
+                if guesses[i] == self.phrase[j]:
+                    print('{}'.format(guesses[i]), end=' ')
+                else:
+                    print('_', end=' ')
+        print()
+
+    def check_letter(self, guess):
+        if guess in self.phrase:
+            return True
+        return False 
+
+    def check_complete(self):
+        pass
