000c410fcb7362d59ed9976c9890f1c0e06b4d8c
Adam Denes <adamdenes@icloud.com>
2020-10-22 11:56:48 +0200

    finishing game logic, making game replayable

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,5 +1,6 @@
 # Create your Game class logic in here.
 import re
+import sys
 import random
 from phrasehunter.phrase import Phrase
 
@@ -52,21 +53,48 @@ class Game():
         while True:
             try:
                 guess = input('\nGuess a letter: ').lower()
-                
+
                 if len(guess) != 1:
-                    raise ValueError('\nOnly one character should be used at a time!')
+                    raise ValueError(
+                        '\nOnly one character should be used at a time!')
                 elif re.match(r'\d+', guess, re.I):
                     raise ValueError('\nNumbers are not allowed!')
                 elif re.match(r'[^\w]', guess, re.I):
-                    raise ValueError('\nPlease only use letter characters! [a-z]')
+                    raise ValueError(
+                        '\nPlease only use letter characters! [a-z]')
+                elif guess in self.guesses:
+                    raise ValueError(
+                        '\n"{}" was already guessed before!'.format(guess))
                 else:
                     return guess
             except ValueError as ve:
                 print('{} Please guess again...'.format(ve))
                 continue
+            except KeyboardInterrupt:
+                print('\nTerminated by player! Exiting...')
+                sys.exit()
 
     def game_over(self):
         if self.missed == 5:
             print('\nGame Over! Better luck next time...')
         else:
             print('\nCongratulations! You Won!')
+
+        while True:
+            try:
+                new_game = input('Do you want to play again? [y/n] ').upper()
+
+                if new_game == 'Y':
+                    ngame = Game()
+                    ngame.start()
+                elif new_game == 'N':
+                    print('See you around!')
+                    sys.exit()
+                else:
+                    raise ValueError('Y or N expected!')
+            except ValueError as ve:
+                print('{} You have typed "{}"...'.format(ve, new_game))
+                continue
+            except KeyboardInterrupt:
+                print('\nTerminated by player! Exiting...')
+                sys.exit()
